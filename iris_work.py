bfc3107d83dff2f26f9d445bd525cb0b3375eff4
Chima Okoro <chimaokoro42@gmail.com>
2022-07-19 12:59:15 +0100

    Added some things

--- iris_work.py
+++ iris_work.py
@@ -1,14 +1,23 @@
 from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+import graphviz
 
 
 
+iris = load_iris()
 
+x = iris.data
+y = iris.target
+tree_clf = DecisionTreeClassifier()
+model = tree_clf.fit(x,y)
+dot_data = export_graphviz(tree_clf, out_file=None, feature_names=iris.feature_names, class_names= iris.target_names,
+                           filled=True, rounded =True, special_characters= True)
 
-
-
-
-
-
+graph = graphviz.Source(dot_data)
+graph.render("iris")
+# print(iris)
+# print(x)
+# print(y)
 
 
 
